3d425f9d9c43328e2a122c5071dffa79f32774f5
Jacob Stanley <jacob@stanley.io>
2015-12-11 18:46:09 +1100

    Try to make text conversion tests more robust

--- test/Icicle/Test/Sea/Text.hs
+++ test/Icicle/Test/Sea/Text.hs
@@ -16,8 +16,6 @@ import           Control.Monad.Trans.Either
 
 import           Data.String (String)
 
-import           Disorder.Core.IO
-
 import           Foreign.C
 import           Foreign.Marshal
 import           Foreign.Ptr
@@ -29,7 +27,9 @@ import           P
 
 import           System.IO
 
-import           Test.QuickCheck (forAllProperties, quickCheckWithResult, stdArgs, maxSuccess)
+import           Test.QuickCheck (forAllProperties, quickCheckWithResult, stdArgs, maxSuccess, forAll)
+import           Test.QuickCheck.Arbitrary (arbitrary)
+import           Test.QuickCheck.Monadic (monadicIO, stop)
 import           Test.QuickCheck.Property (Property, (===), counterexample, failed, property)
 
 import           X.Control.Monad.Trans.Either (firstEitherT)
@@ -37,26 +37,43 @@ import           X.Control.Monad.Trans.Either (firstEitherT)
 
 prop_text_read_int :: Int64 -> Property
 prop_text_read_int expected
- = testRead "testable_text_read_iint" (show expected)
- $ \actual -> actual === expected
+ = testRead "testable_text_read_iint" (show expected) $ \actual ->
+   actual === expected
 
-prop_text_read_double :: Double -> Property
-prop_text_read_double expected
- = testRead "testable_text_read_idouble" (show expected)
- $ \actual -> let diff    = actual - expected
-                  epsilon = 0.000000001
-              in counterexample ("expected   = " <> show expected)
-               $ counterexample ("actual     = " <> show actual)
-               $ counterexample ("difference = " <> show diff)
-               $ property (diff < epsilon)
+prop_text_read_double :: Property
+prop_text_read_double
+ = forAll arbitrary $ \(expected :: Double) ->
+   testRead "testable_text_read_idouble" (show expected) $ \actual ->
+     let diff    = actual - expected
+
+         epsilon = if expected /= 0
+                   then abs expected * 1.0e-14
+                   else 0
+
+         equal   = isNaN expected && isNaN actual
+                || expected == actual
+                || diff < epsilon
+
+     in counterexample ("expected   = " <> show expected)
+      $ counterexample ("actual     = " <> show actual)
+      $ counterexample ("difference = " <> show diff)
+      $ counterexample ("epsilon    = " <> show epsilon)
+      $ property equal
 
 seaTestables :: SourceCode
 seaTestables = codeOfDoc $ PP.vsep
-  [ "ierror_msg_t testable_text_read_iint (char *p, size_t n, iint_t *output_ptr) {"
-  , "    return text_read_iint (&p, p + n, output_ptr);"
+  [ "ierror_msg_t from_loc (char *ps, char *pe, ierror_loc_t loc) {"
+  , "    if (!loc) return 0;"
+  , "    loc->line_start = ps;"
+  , "    loc->line_end   = pe;"
+  , "    return ierror_loc_pretty (loc, 1);"
+  , "}"
+  , "ierror_msg_t testable_text_read_iint (char *p, size_t n, iint_t *output_ptr) {"
+  , "    return from_loc (p, p+n, text_read_iint (&p, p+n, output_ptr));"
   , "}"
   , "ierror_msg_t testable_text_read_idouble (char *p, size_t n, idouble_t *output_ptr) {"
-  , "    return text_read_idouble (&p, p + n, output_ptr);"
+  , "    segv_install_handler (0, 0);"
+  , "    return from_loc (p, p+n, text_read_idouble (&p, p+n, output_ptr));"
   , "}"
   ]
 
@@ -67,11 +84,11 @@ data TestError
  | JetskiError JetskiError
 
 testRead :: Storable a => Symbol -> String -> (a -> Property) -> Property
-testRead symbol input onOutput = testIO . runRight $ do
+testRead symbol input onOutput = monadicIO . (stop =<<) . liftIO . runRight $ do
   lib <- firstEitherT JetskiError (readLibrary seaTestables)
   fn  <- firstEitherT JetskiError (function lib symbol (retPtr retCChar))
 
-  res <- liftIO $
+  (hoistEither =<<) . liftIO $
     withCStringLen input $ \(p, n) -> do
     alloca $ \(outputPtr :: Ptr a) -> do
       errorPtr <- liftIO $ fn [argPtr p, argCSize (fromIntegral n), argPtr outputPtr]
@@ -84,8 +101,6 @@ testRead symbol input onOutput = testIO . runRight $ do
       else do
         output <- peek outputPtr
         return (Right (counterexample ("input = " <> show input) (onOutput output)))
-
-  hoistEither res
 
 runRight :: Monad m => EitherT TestError m Property -> m Property
 runRight a = do
